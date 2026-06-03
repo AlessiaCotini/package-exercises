2d6939c5baed3bd37bcdac7a68ecfa7ce0b5a45c
AlessiaCotini <alessia.cotini@icloud.com>
2026-06-03 14:06:50 +0200

    uno finito

--- src/eserciziouno/PrimoEsercizio.java
+++ src/eserciziouno/PrimoEsercizio.java
@@ -7,10 +7,17 @@ public class PrimoEsercizio {
             Scanner scanner= new Scanner(System.in);
             System.out.println("inserire una parola");
             String parola = scanner.nextLine();
-            String risultato = pariodispari(parola);
-            System.out.println("la tua parola :" + risultato);
+            System.out.println("la tua parola :" + pariodispari(parola));
+            System.out.println("inserire un anno");
+            int anno = scanner.nextInt();
+            System.out.println("L'anno inserito risulta :" + annoBisestile(anno));
     }
     public static String pariodispari(String parola){
         return (parola.length()%2 == 0)? "LUNGHEZZA PARI" : "LUNGHEZZA DISPARI";
+    }
+    public static String annoBisestile (int anno){
+            if (anno %4== 0){
+            return ((anno/400/100 ) == 0)? "ANNO BISESTILE" : "ANNO NON BISESTILE";}
+            else {return "ANNO NON BISESTILE";}
     }
 }
