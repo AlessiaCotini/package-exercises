bb24299aed62d3ff20b53ae208eb749ac97081b3
AlessiaCotini <alessia.cotini@icloud.com>
2026-06-03 15:10:17 +0200

    4

---
+++ src/esercizioquattro/QuartoEsercizio.java
@@ -0,0 +1,18 @@
+package esercizioquattro;
+
+import java.sql.Array;
+import java.util.Scanner;
+
+public class QuartoEsercizio {
+    static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("inserire un numero");
+        int inserito = scanner.nextInt();
+        System.out.println("Secondi al lancio :" + inserito);
+
+    }
+   public static String lancioSullaLuna (int inserito){
+    return "ok";
+
+   }
+}
